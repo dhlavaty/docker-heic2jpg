bc62082e0e47bfa812ee7fb82a677144c68e73ec
Renoir Boulanger <hello@renoirboulanger.com>
2022-06-02 11:17:44 -0400

    fix: Ability to match UPPERCASE extensions

--- heic2jpg.sh
+++ heic2jpg.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 src=$1
-size=$(find $src -type f | grep heic | wc -l)
+size=$(find $src -type f | grep -i '\.heic$' | wc -l)
 cnt=0
 
-for i in $(find $src -type f | grep heic)
+for i in $(find $src -type f | grep -i '\.heic$')
 do
-    ((cnt=cnt+1))
-	target=$(echo $i | sed 's/heic$/jpg/')
+	((cnt=cnt+1))
+	target=$(echo $i | sed 's/heic$/jpg/i')
 	printf "$i convert to $target\n"
 	/usr/bin/convert $i $target
 	printf " [$cnt/$size]\r"
